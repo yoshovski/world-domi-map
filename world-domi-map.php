f7795bc18850a61de5e8f372c74d4dc313a545f4
yoshovski <stefan.yoshovski@gmail.com>
2024-04-20 17:20:53 +0200

    refactor: Reorganize project structure

--- world-domi-map.php
+++ world-domi-map.php
@@ -10,23 +10,23 @@
 require_once(plugin_dir_path(__FILE__) . 'includes/activation.php');
 require_once(plugin_dir_path(__FILE__) . 'includes/map-generator.php');
 require_once(plugin_dir_path(__FILE__) . 'includes/shortcode.php');
-require_once(plugin_dir_path(__FILE__) . 'includes/admin/settings-page.php');
-require_once(plugin_dir_path(__FILE__) . 'includes/admin/settings-page-validation.php');
+require_once(plugin_dir_path(__FILE__) . 'includes/admin/pages/settings-page.php');
+require_once(plugin_dir_path(__FILE__) . 'includes/admin/pages/settings-page-validation.php');
 require_once(plugin_dir_path(__FILE__) . 'includes/data.php');
 
 
 // Enqueue plugin scripts and styles for the admin area
 function wdm_enqueue_admin_scripts() {
     // Register and enqueue admin styles
-    wp_register_style('wdm-admin-style', plugins_url('css/wdm_style.css', __FILE__));
+    wp_register_style('wdm-admin-style', plugins_url('/assets/css/wdm_style.css', __FILE__));
     wp_enqueue_style('wdm-admin-style');
 
     // Register and enqueue admin scripts
-    wp_register_script('wdm-admin-script', plugin_dir_url(__FILE__) . 'js/wdm_admin.js', array('jquery'));
+    wp_register_script('wdm-admin-script', plugin_dir_url(__FILE__) . '/includes/admin/js/wdm_admin.js', array('jquery'));
     wp_enqueue_script('wdm-admin-script');
 
     // Register and enqueue common script
-    wp_register_script('wdm-common-script', plugin_dir_url(__FILE__) . 'js/wdm_common.js', array('jquery'));
+    wp_register_script('wdm-common-script', plugin_dir_url(__FILE__) . '/assets/js/wdm_common.js', array('jquery'));
     wp_enqueue_script('wdm-common-script');
 }
 add_action('admin_enqueue_scripts', 'wdm_enqueue_admin_scripts');
@@ -34,15 +34,15 @@ add_action('admin_enqueue_scripts', 'wdm_enqueue_admin_scripts');
 // Enqueue plugin scripts and styles for the front-end
 function wdm_enqueue_scripts() {
     // Register and enqueue front-end styles
-    wp_register_style('wdm-style', plugins_url('css/wdm_style.css', __FILE__));
+    wp_register_style('wdm-style', plugins_url('/assets/css/wdm_style.css', __FILE__));
     wp_enqueue_style('wdm-style');
 
     // Register and enqueue front-end scripts
-    wp_register_script('wdm-script', plugins_url('js/wdm_frontend.js', __FILE__), array('jquery'));
+    wp_register_script('wdm-script', plugins_url('/public/js/wdm_frontend.js', __FILE__), array('jquery'));
     wp_enqueue_script('wdm-script');
 
     // Register and enqueue common script
-    wp_register_script('wdm-common-script', plugin_dir_url(__FILE__) . 'js/wdm_common.js', array('jquery'));
+    wp_register_script('wdm-common-script', plugin_dir_url(__FILE__) . '/assets/js/wdm_common.js', array('jquery'));
     wp_enqueue_script('wdm-common-script');
 }
 add_action('wp_enqueue_scripts', 'wdm_enqueue_scripts');
