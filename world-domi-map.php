406f3f046f606a0665dbc3d89c7489bb290169d7
yoshovski <stefan.yoshovski@gmail.com>
2024-04-24 12:42:22 +0200

    security: load scripts in footer and add version of style and sripts

--- world-domi-map.php
+++ world-domi-map.php
@@ -20,16 +20,18 @@ require_once(plugin_dir_path(__FILE__) . 'includes/activation.php');
 require_once(plugin_dir_path(__FILE__) . 'includes/admin/pages/settings-page.php');
 require_once(plugin_dir_path(__FILE__) . 'includes/admin/pages/registration.php');
 
+define('WDM_VERSION', '1.0.0');
+
 $wdm_data = WDM_Data::getInstance();
 $wdm_shortcode = WDM_Shortcode::getInstance();
 
 // Enqueue plugin scripts and styles for the common area
 function wdm_enqueue_common_scripts() {
     // Register and enqueue front-end styles
-    wp_register_style('wdm-style', plugins_url('/assets/css/wdm_style.css', __FILE__));
+    wp_register_style('wdm-style', plugins_url('/assets/css/wdm_style.css', __FILE__), array(), WDM_VERSION);
     wp_enqueue_style('wdm-style');
 
-    wp_register_script('wdm-ajax', plugin_dir_url(__FILE__) . '/assets/js/wdm-ajax.js', array('jquery'), '1.0.0', true);
+    wp_register_script('wdm-ajax', plugin_dir_url(__FILE__) . '/assets/js/wdm-ajax.js', array('jquery'), WDM_VERSION, true);
     wp_enqueue_script('wdm-ajax');
     wp_localize_script('wdm-ajax', 'wdm_ajax_object', array(
         'ajax_url' => admin_url('admin-ajax.php'),
@@ -37,7 +39,7 @@ function wdm_enqueue_common_scripts() {
     ));
 
     // Register and enqueue common script
-    wp_register_script('wdm-common-script', plugin_dir_url(__FILE__) . '/assets/js/wdm.js', array('jquery'));
+    wp_register_script('wdm-common-script', plugin_dir_url(__FILE__) . '/assets/js/wdm.js', array('jquery'), WDM_VERSION, true);
     wp_enqueue_script('wdm-common-script');
 
 }
@@ -47,12 +49,12 @@ function wdm_enqueue_admin_scripts() {
     wdm_enqueue_common_scripts();
 
     // Register and enqueue admin styles
-    wp_register_style('wdm-admin-style', plugins_url('/includes/admin/css/wdm-admin-style.css', __FILE__));
+    wp_register_style('wdm-admin-style', plugins_url('/includes/admin/css/wdm-admin-style.css', __FILE__), array(), WDM_VERSION);
     wp_enqueue_style('wdm-admin-style');
 
 
     // Register and enqueue admin ajax script
-    wp_register_script('wdm-admin-ajax', plugin_dir_url(__FILE__) . '/includes/admin/js/wdm-admin-ajax.js', array('jquery'));
+    wp_register_script('wdm-admin-ajax', plugin_dir_url(__FILE__) . '/includes/admin/js/wdm-admin-ajax.js', array('jquery'), WDM_VERSION, true);
     wp_enqueue_script('wdm-admin-ajax');
     wp_localize_script('wdm-admin-ajax', 'wdm_admin_ajax_object', array(
         'ajax_url' => admin_url('admin-ajax.php'),
@@ -60,7 +62,7 @@ function wdm_enqueue_admin_scripts() {
     ));
 
     // Register and enqueue admin scripts
-    wp_register_script('wdm-admin-script', plugin_dir_url(__FILE__) . '/includes/admin/js/wdm-admin.js', array('jquery'));
+    wp_register_script('wdm-admin-script', plugin_dir_url(__FILE__) . '/includes/admin/js/wdm-admin.js', array('jquery'), WDM_VERSION, true);
     wp_enqueue_script('wdm-admin-script');
 }
 
@@ -69,7 +71,7 @@ function wdm_enqueue_public_scripts() {
     wdm_enqueue_common_scripts();
 
     // Register and enqueue front-end scripts
-    wp_register_script('wdm-script', plugin_dir_url(__FILE__) . '/includes/public/js/wdm_frontend.js', array('jquery'));
+    wp_register_script('wdm-script', plugin_dir_url(__FILE__) . '/includes/public/js/wdm_frontend.js', array('jquery'), WDM_VERSION, true);
     wp_enqueue_script('wdm-script');
 
 }
@@ -79,13 +81,17 @@ function wdm_block_scripts() {
     wp_register_script(
         'wdm-block-render-map',
         plugins_url( '/assets/js/wdm.js', __FILE__ ),
-        array( 'wp-blocks', 'wp-element', 'wp-editor' )
+        array( 'wp-blocks', 'wp-element', 'wp-editor' ),
+        WDM_VERSION,
+        true
        );
 
     wp_register_script(
         'wdm-block-map',
         plugins_url( 'includes/admin/js/wdm-admin-block.js', __FILE__ ),
-        array( 'wp-blocks', 'wp-element', 'wp-editor' )
+        array( 'wp-blocks', 'wp-element', 'wp-editor' ),
+        WDM_VERSION,
+        true
     );
 
     wp_localize_script('wdm-block-map', 'wdm_block_map_data',
